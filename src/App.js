d35b4e400566ac250561be91d8c779c8d0b0af30
will <wmashhadi@Gmail.com>
2022-05-20 13:48:46 +0100

    removing unused variables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { useState } from 'react';
-import HeroSection from './components/HeroSection';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
@@ -15,7 +13,7 @@ function App() {
     <>
     <Router>
     <Navbar/>
-    {/* <Home /> */}
+    
       <Routes>
           <Route  path='/' exact element={<Home/>} />
           <Route path='/services' element={<Services/>} />
